fbfdb394782f47f6994f625e0b2e10cf6917cc79
jang <dkjang@naver.com>
2020-08-05 20:36:19 +0900

    ClickMe Button

--- app/src/main/java/com/kakao/kitlinbasic_20200805/MainActivity.kt
+++ app/src/main/java/com/kakao/kitlinbasic_20200805/MainActivity.kt
@@ -2,10 +2,20 @@ package com.kakao.kitlinbasic_20200805
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        ctrl+/ : 주석처리
+        clickMeBtn.setOnClickListener {
+//            중괄호내부코드 클릭할경우 실행코드
+            Log.d("버튼클릭됨","ClickMe버튼눌림!")
+        }
+
+
     }
 }
