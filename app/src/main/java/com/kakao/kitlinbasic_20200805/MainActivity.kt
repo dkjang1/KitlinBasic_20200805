b0178207a43a763ac13b3db9946a9d0d4ab8eaa0
jang <dkjang@naver.com>
2020-08-05 21:01:47 +0900

    Toast Show

--- app/src/main/java/com/kakao/kitlinbasic_20200805/MainActivity.kt
+++ app/src/main/java/com/kakao/kitlinbasic_20200805/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kakao.kitlinbasic_20200805
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +15,14 @@ class MainActivity : AppCompatActivity() {
         clickMeBtn.setOnClickListener {
 //            중괄호내부코드 클릭할경우 실행코드
             Log.d("버튼클릭됨","ClickMe버튼눌림!")
+            Toast.makeText(this,"ClickMe버튼이 눌림", Toast.LENGTH_SHORT).show()
         }
 
         secondBtn.setOnClickListener {
             Log.d("버튼눌림","두번째버튼눌림!!")
+//            Log.d("로그종류","로그내용") log debug
+//            Log.e("에러로그","에러내용") log error
+//            Log.i("정보로그","정보내용") log information
         }
 
     }
